d1c1fe8e4414146afe536d8298c91aaf56f0cb0f
DESKTOP-U98AFIT\minht <20520842@gm.uit.edu.vn>
2022-08-08 23:40:39 +0700

    transfer image

--- application.py
+++ application.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+import os
 application = Flask(__name__)
+
+CORS(application)
+application.config['CORS_HEADERS'] = 'Content-Type'
+application.config['UPLOAD_FOLDER'] = "static"
 
 @application.route("/", methods = ['GET','POST'])
 def hello():
   if request.method == "POST":
-    a = int(request.form['so_phong'])
-    print(a)
-    gia = a*60000000+ a*a*1000000 + a*1.24135 + 0.9724
-    return render_template("index.html", g = 'gia can de xay tro la ' + str(gia))
+    image = request.files['file']
+    if image:
+      path_to_save = os.path.join(application.config['UPLOAD_FOLDER'], image.filename)
+      image.save(path_to_save)
+      return render_template("index.html", link = request.url + path_to_save)
   else:
-    return render_template("index.html", g = '')
+    return render_template("index.html", link = '')
 
 
 if __name__ == "__main__":
-  application.run()
+  application.run(debug = True)
